3e6bb8809ce13917e38333ee63d77543961c5eb8
Mike Purvis <mikedotexe@gmail.com>
2023-02-04 14:56:03 -0800

    changes to match beta/split-0.0.0 branch of contracts

--- call.sh
+++ call.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-junod tx wasm execute juno1782gkddttuvay4rppcw67f2g4mfluvxdjs0pjysuqvzjq4gqx5hs9qrpa4 '{"make_croncat_task":{"croncat_manager_address":"juno174ncqgapq7fudqj64ut4ue47gevlqp93guecjelnkquruvnpjdgsuk046w","boolean_address":"juno1wqhttvnv8xcqs55wasu2s49dxmgyx5jpygp4n9p0tndc8kx7nenqsr0ev3"}}' --amount 1000000ujunox --from mikereg --node https://juno-testnet-rpc.polkachu.com:443 --chain-id uni-5 --gas-prices 0.025ujunox --gas auto --gas-adjustment 1.3 -b block -y -o json | jq | head -n 42
+starsd tx wasm execute stars13nl2lqe3gesdf7ld0z0fa9sadt5mgy98tq7xre53pnw5eyhs2pmsze3ffv '{"make_croncat_task":{"croncat_factory_address":"stars1frqpxtgdhq7psq9u97v32kc69zeq0etal8vmnmp0st68ye5928jq8437kv","boolean_address":"stars10skdjcaeqpn84fxssasd352nkwkgxd88mrxz9cvkdvj0tq62s0wq2re5nq"}}' --amount 1000000ustars --gas-prices 0.025ustars --gas-adjustment 1.7 --gas auto -b block --from mikereg -o json -y | jq | head -n 42
